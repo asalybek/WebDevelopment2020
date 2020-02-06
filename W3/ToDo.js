140d903efa300a81ad85b14d8c24ab02eb197fee
asalybek <luxuryteenager@gmail.com>
2020-02-07 00:37:08 +0600

    week 3

---
+++ W3/ToDo.js
@@ -0,0 +1,25 @@
+var plan = document.querySelector('.input');
+var uList = document.querySelector('.plans');
+
+var addButton = document.getElementById('addBtn');
+
+var deleteButton = document.getElementById('deleteBtn');
+
+function addItemToTheList() {
+  var newItem = document.createElement("li");
+  if (plan.value != ''){
+  newItem.innerHTML = plan.value;
+  plan.value = "";
+  uList.appendChild(newItem);
+}else {
+    alert('Fill the input!!!');
+  }
+}
+function deleteItem(){
+  if (uList.hasChildNodes()) {
+    uList.removeChild(uList.firstChild);
+  }
+  else {
+    alert('Any Plans yet to complete');
+    }
+}
